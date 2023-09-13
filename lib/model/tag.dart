282c50d59a5bffad8d1df3d8ee0a310d76846345
ahmedjarad0 <0597181403anj@gmail.com>
2023-09-13 20:41:56 +0300

    tag model

---
+++ lib/model/tag.dart
@@ -0,0 +1,47 @@
+class TagModel {
+  List<Tags>? tags;
+
+  TagModel({this.tags});
+
+  TagModel.fromJson(Map<String, dynamic> json) {
+    if (json['tags'] != null) {
+      tags = <Tags>[];
+      json['tags'].forEach((v) {
+        tags!.add(new Tags.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.tags != null) {
+      data['tags'] = this.tags!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Tags {
+  int? id;
+  String? name;
+  String? createdAt;
+  String? updatedAt;
+
+  Tags({this.id, this.name, this.createdAt, this.updatedAt});
+
+  Tags.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
+  }
+}
