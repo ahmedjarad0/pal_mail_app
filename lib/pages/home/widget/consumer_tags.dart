8b55d3d47a8aabb9e84c8bb4c1de5a5293dbc66f
ahmedjarad0 <0597181403anj@gmail.com>
2023-09-13 21:06:29 +0300

    add consumer tag provider

---
+++ lib/pages/home/widget/consumer_tags.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../../../provider/tag_provider.dart';
+import '../../widgets/custom_action_chip.dart';
+class ConsumerTags extends StatelessWidget {
+  const ConsumerTags({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(18),
+      margin:
+      const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30.r)),
+      child: Consumer<TagProvider>(
+        builder: (context, tagProvider, child) {
+          if (tagProvider.statues == TagStatues.Loading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final tags = tagProvider.tag;
+          if (tags != null &&
+              tagProvider.statues == TagStatues.Loaded) {
+            return Wrap(
+              spacing: 8,
+              children: [
+                const CustomActionChip(
+                  titleTag: 'All Tag',
+                ),
+                for (int i = 0; i < tags.length; i++) ...[
+                  CustomActionChip(titleTag: tags[i].name!),
+                ]
+              ],
+            );
+          }
+          if (tagProvider.statues == TagStatues.Error) {
+            return const Center(
+              child: Text('Error Data'),
+            );
+          } else {
+            return const Center(
+              child: Text('No Data'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
