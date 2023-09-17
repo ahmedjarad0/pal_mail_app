99d4e1c9cddca623193c4a82174b55f1460f3764
ahmedjarad0 <0597181403anj@gmail.com>
2023-09-18 01:02:54 +0300

    custom show model bottom

---
+++ lib/pages/widgets/custom_show_model_bottom.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class CustomShowModelBottom extends StatefulWidget {
+  const CustomShowModelBottom({
+    super.key, required this.builder, this.childModel,
+  });
+final  Widget Function(BuildContext, ScrollController) builder ;
+final Widget ?childModel ;
+
+  @override
+  State<CustomShowModelBottom> createState() => _CustomShowModelBottomState();
+}
+
+class _CustomShowModelBottomState extends State<CustomShowModelBottom> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.deferToChild,
+      onTap: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+          useSafeArea: true,
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(30.r),
+            ),
+          ),
+          context: context,
+          builder: (context) {
+            return DraggableScrollableSheet(
+              initialChildSize: 0.99,
+              maxChildSize: 0.99,
+              minChildSize: 0.99,
+              expand: true,
+              builder: widget.builder,
+            );
+          },
+        );
+      },
+      child: widget.childModel
+    );
+  }
+}
